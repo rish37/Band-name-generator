d70793f481cd355e0c6b9d5d5d92054627661ea4
rishikesh vishwakarma <80611979+rish37@users.noreply.github.com>
2023-08-27 12:22:47 +0530

    Add files via upload

---
+++ Band name generator.py
@@ -0,0 +1,23 @@
+print('''
+  ____                  _                               
+ |  _ \                | |                              
+ | |_) | __ _ _ __   __| |  _ __   __ _ _ __ ___   ___  
+ |  _ < / _` | '_ \ / _` | | '_ \ / _` | '_ ` _ \ / _ \ 
+ | |_) | (_| | | | | (_| | | | | | (_| | | | | | |  __/ 
+ |____/ \__,_|_| |_|\__,_| |_| |_|\__,_|_| |_| |_|\___| 
+                                 | |                    
+   __ _  ___ _ __   ___ _ __ __ _| |_ ___  _ __         
+  / _` |/ _ \ '_ \ / _ \ '__/ _` | __/ _ \| '__|        
+ | (_| |  __/ | | |  __/ | | (_| | || (_) | |           
+  \__, |\___|_| |_|\___|_|  \__,_|\__\___/|_|           
+   __/ |                                                
+  |___/                                                                                     
+''')
+print('Welcome to Band Name Generator')
+
+cityName = input('Which district are you from: ')
+print(cityName)
+
+petName = input('What is a name of your pets: ')
+print(petName)
+print(f'your Band Name will be : {cityName} {petName}')
